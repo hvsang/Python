d9cc4b2090800b7b44680a2533b16c956ca61a3f
sang <sang.ho@pyramid-vn.com>
2025-09-05 15:18:17 +0700

    Clean code

--- calibration_tool/devices/field_device.py
+++ calibration_tool/devices/field_device.py
@@ -1,11 +1,12 @@
 from utils.file_utils import format_file
 from logic.main_logic import result
+from utils.calc_utils import compute_input
 
 
 def field_device(channel_number, total_samples, range_select, device_config):
     full_scale_range = device_config["max_current_range"]
     channel_size = device_config["channel_number"]
-    input_current = device_config["source_keithley"]
+    input_current = compute_input(full_scale_range)
     time_delay_keithley = device_config["time_set_keithley"]
     time_delay_get_data = device_config["time_get_data"]
     device_scale = 1
--- calibration_tool/devices/voltage_device.py
+++ calibration_tool/devices/voltage_device.py
@@ -1,10 +1,11 @@
 from utils.file_utils import format_file
 from logic.main_logic import result
+from utils.calc_utils import compute_input
 
 
 def voltage_device(channel_number, total_samples, range_select, device_config):
     full_scale_range = device_config["max_current_range"][f"{range_select}"]
-    input_current = device_config["source_keithley"][f"{range_select}"]
+    input_current = compute_input(full_scale_range)
     channel_size = device_config["channel_number"]
     time_delay_keithley = device_config["time_set_keithley"]
     time_delay_get_data = device_config["time_get_data"]
